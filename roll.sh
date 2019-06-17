5499f235f1bd62017408f6af5c2cbdedbc84e2e2
Andy Manoske <andy@hashicorp.com>
2019-06-17 14:17:32 -0700

    Add files via upload

---
+++ roll.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Dice Rolling Program
+
+while getopts f:n:b: option
+do
+case "${option}"
+in
+f) FACES=${OPTARG};;
+n) NUMTIMES=${OPTARG};;
+b) ADDBONUS=${OPTARG};;
+esac
+done
+
+echo "    /                     "
+echo "O===[====================- Rolling $NUMTIMES d$FACES..."
+echo "    \                     "
+
+for (( i=1; i<=NUMTIMES; i++))
+do
+	roll=$((1+RANDOM % FACES))
+    bonusRoll=$((roll+ADDBONUS))
+	echo "Rolled a d$FACES for $roll (with $ADDBONUS, $bonusRoll)"
+done 
